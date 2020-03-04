d4420a8001481546c09aaa31fc5751b0cee9db73
Jonathan Kerr CS2018 <jonathan.kerr.2018@uni.strath.ac.uk>
2020-03-04 15:37:51 +0000

    Created "Cryptogram" class

---
+++ Cryptogram.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Cryptogram {
+	
+	String phrase = "HelloWorld";  //The phrase in which we will encrypt
+	
+	
+	
+	LetterCryptogram Crypto = new LetterCryptogram(phrase);
+
+	
+}
