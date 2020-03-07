5c5bc070e72fc773108b4b0b2a50ad745d975bc6
haresm <hmahmood229@gmail.com>
2020-03-07 09:50:05 +0000

    Update "Game" class.

--- src/src/Game.java
+++ src/src/Game.java
@@ -1,19 +1,23 @@
+/**
+ * Main driver class to run game.
+ */
 public class Game
 { 
-	
+	/**
+	 *  Generates a new cryptogram.
+	 */
+	public static void generateCryptogram() 
+	{
+		Cryptogram n = new Cryptogram();
+	}
+
+	/**
+	 * Main method.
+	 * 
+	 * @param args command-line arguents.
+	 */
 	public static void main(String [] args)
 	{
-	
 		generateCryptogram();
-		
 	}
-	
-	
-	
-	public static void generateCryptogram() {
-		Cryptogram n = new Cryptogram();
-		
-	}
-
-
 }
