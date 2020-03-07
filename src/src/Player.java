4c287da507357f5377d9cae9c12f75c97e836b71
haresm <hmahmood229@gmail.com>
2020-03-07 09:52:48 +0000

    Update "Player" class.
    "master" branch now up to date with "Sprint1" branch.

--- src/src/Player.java
+++ src/src/Player.java
@@ -1,3 +1,6 @@
+/**
+ * Represents a user who plays the game.
+ */
 public class Player 
 {
 	private String username;
@@ -6,31 +9,57 @@ public class Player
 	private int playedCryptograms;
 	private int completedCryptograms;
 	
+	/**
+	 * Gets player's accuracy.
+	 * 
+	 * @return player's accuracy.
+	 */
 	public float getAccuracy()
 	{
 		return accuracy;
 	}
 	
+	/**
+	 * Sets player's accuracy.
+	 * 
+	 * @param accuracy target accuracy.
+	 */
 	public void setAccuracy(int accuracy)
 	{
 		this.accuracy = accuracy;
 	}
 	
+	/**
+	 * Gets number of cryptograms played.
+	 * 
+	 * @return number of cryptograms played.
+	 */
 	public int getPlayedCryptograms()
 	{
 		return playedCryptograms;
 	}
 	
+	/**
+	 * Increments the number of cryptograms played.
+	 */
 	public void incrementPlayedCryptograms()
 	{
 		playedCryptograms++;
 	}
 	
+	/**
+	 * Gets the number of cryptograms completed.
+	 * 
+	 * @param completedCryptograms the number of cryptograms completed.
+	 */
 	public int getCompletedCryptograms()
 	{
 		return completedCryptograms;
 	}
 	
+	/**
+	 * Increments the number of cryptograms completed.
+	 */
 	public void incrementCompletedCryptograms()
 	{
 		completedCryptograms++;
