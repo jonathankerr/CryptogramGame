63f1d5e2f909880a11419833024cb2c9acff25b0
haresm <hmahmood229@gmail.com>
2020-03-11 15:50:42 +0000

    Update with latest version.

--- src/src/Cryptogram.java
+++ src/src/Cryptogram.java
@@ -1,8 +1,128 @@
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
- * Base class for cryptograms.
  */
 public class Cryptogram 
 {	
-	String phrase = "HelloWorld";  //The phrase in which we will encrypt
-	LetterCryptogram cgram = new LetterCryptogram(phrase);
+	protected char[] alphabet =
+	{ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+		'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' 
+	};
+
+	private ArrayList<Character> alphabetList;
+	private ArrayList<Character> uniqueChars; // List of unique character in the phrase.
+	private ArrayList<Character> uniqueEncryptedChars; // List of unique character in the phrase.
+	private HashMap<Character, Character> charMap;
+
+	protected String phrase;
+	private String encryptedPhrase;
+
+	public Cryptogram(String phrase)
+	{
+		alphabetList = new ArrayList<Character>();
+		uniqueChars = new ArrayList<Character>();
+		uniqueEncryptedChars = new ArrayList<Character>();
+		charMap = new HashMap<Character, Character>();
+
+		this.phrase = phrase;
+
+		for (char c : alphabet) 
+		{
+			alphabetList.add(c);
+		}
+	}
+
+	public boolean getCompletionStatus(int currentCharIndex, HashMap<Character, Character> userGuesses)
+	{
+		boolean isCompleted = true;
+
+		for (Character c : charMap.keySet())
+		{
+			char value = Character.toLowerCase(userGuesses.get(charMap.get(c)));  
+
+			if (c != value)
+			{
+				isCompleted = false;
+	
+				break;
+			}
+		} 
+		
+
+		return isCompleted;
+	}
+
+	public char getChar(int currentCharIndex)
+	{
+		return Character.toUpperCase(uniqueChars.get(currentCharIndex));
+	}
+
+	public char getEncryptedChar(int currentCharIndex)
+	{
+		return Character.toUpperCase(uniqueEncryptedChars.get(currentCharIndex));
+	}
+
+	public ArrayList<Character> getUniqueChars()
+	{
+		return uniqueChars;
+	}
+
+	public String getEncryptedPhrase()
+	{
+		return encryptedPhrase.replaceAll(".(?!$)", "$0 ");
+	}
+
+	/**
+	 * Maps each character in the phrase to a letter from the alphabet.
+	 */
+	public void createMapping() 
+	{
+		StringBuilder builder = new StringBuilder(phrase.length());
+
+		for (char c : phrase.toCharArray()) 
+		{
+			if (!uniqueChars.contains(c))
+			{
+				uniqueChars.add(c);
+			}
+		}
+		
+		Collections.shuffle(alphabetList);
+		
+		// For each of the unique letters, assign a random letter as it's encypted pair.
+		for (char c : uniqueChars) 
+		{  
+			charMap.put(c, alphabetList.get(0));
+			alphabetList.remove(0);
+		}
+		
+		// Prevents any letters being mapped to themselves.
+		for (char c : charMap.keySet()) 
+		{
+			char currentValue = charMap.get(c);
+			
+			if (c == currentValue) 
+			{
+				charMap.put(c, alphabetList.get(0));
+				alphabetList.remove(0);
+				alphabetList.add(currentValue);
+			}
+		}
+	
+		// Construct string from encrypted characters.
+		for (char c : phrase.toCharArray()) 
+		{
+			if (!uniqueEncryptedChars.contains(charMap.get(c)))
+			{
+				uniqueEncryptedChars.add(charMap.get(c));
+			}
+
+			builder.append(charMap.get(c));
+		}
+
+		encryptedPhrase = builder.toString();
+	}
+
 }
--- src/src/Game.java
+++ src/src/Game.java
@@ -1,16 +1,184 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
  * Main driver class to run game.
  */
 public class Game
 { 
-	/**
-	 *  Generates a new cryptogram.
-	 */
-	public static void generateCryptogram() 
+	//private Player currentPlayer;
+	private static Cryptogram cryptogram;
+
+	private HashMap<Character, Character> userGuesses;
+
+	private int currentCharIndex;
+
+	private Game()
 	{
-		Cryptogram n = new Cryptogram();
+		userGuesses = new HashMap<Character, Character>();
+
+		currentCharIndex = 0;
 	}
 
+	/**
+	 * Starts the game and gets input from terminal.
+	 */
+	private void start() 
+	{
+		System.out.println("\nHello player, welcome to our cryptogram program!");
+		System.out.println("You can type the foloowing commands:\n- \"undo\"\n- \"clear\"\n- \"exit\"\n");
+		System.out.println("Please type \"generate\" to start...\n");
+
+		Scanner input = new Scanner(System.in);
+		String userInput = input.nextLine();
+		//input.close();
+
+		if (userInput.equals("exit"))
+		{
+			System.exit(0);
+		}
+
+		while (!userInput.equals("generate"))
+		{
+			System.out.println("Invalid input, please type \"generate\" to start...\n");
+			userInput = input.nextLine();
+
+			if (userInput.equals("exit"))
+			{
+				System.exit(0);
+			}
+		}	
+
+		System.out.println();
+
+		while (!completed())
+		{
+			System.out.print("\nYour cryptogram is:");
+			System.out.println("\n" + cryptogram.getEncryptedPhrase());
+			getMapping();
+
+			System.out.println("Please enter a singular character value for cryptogram character \'" + cryptogram.getEncryptedChar(currentCharIndex) + "\'...\n");
+
+			//input = new Scanner(System.in);
+			userInput = input.nextLine();
+			char userGuess = ' ';
+
+			boolean inputRead = readInput(userInput);
+			
+			if (!inputRead && (userInput.length() > 1 || userInput.length() == 0)) 
+			{
+				System.out.println("Invalid input. Please enter a singular character value for cryptogram character \'" + cryptogram.getEncryptedChar(currentCharIndex) + "\'...\n");
+			}
+			else if (!inputRead)
+			{
+				userGuess = userInput.charAt(0);
+				userGuesses.put(cryptogram.getEncryptedChar(currentCharIndex), userGuess);
+				currentCharIndex++;
+			}
+		}
+	}
+
+	/**
+	 * Gets user input from terminal.
+	 */
+	private boolean readInput(String userInput) 
+	{
+		if (userInput.equals("undo"))
+		{
+			undo();
+			return true;
+		}
+		else if (userInput.equals("clear"))
+		{
+			clear();
+			return true;
+		}
+			
+		return false;
+	}
+
+	/**
+	 * Prints out the generated cryptogram.
+	 */
+	private void getMapping() 
+	{
+		if (currentCharIndex == 0) 
+		{
+			System.out.println();
+		}
+		else
+		{
+			for (int i = 0; i < cryptogram.getEncryptedPhrase().length(); i++) 
+			{
+				System.out.print('-');
+			}
+
+			System.out.println();
+
+			for (char c : cryptogram.getEncryptedPhrase().toCharArray()) 
+			{
+				char output = userGuesses.containsKey(c) ? userGuesses.get(c) : ' ';
+				System.out.print(Character.toUpperCase(output));
+			}
+		
+			System.out.println("\n");
+		}
+	}
+
+	/**
+	 * Undoes an action.
+	 */
+	private void undo() 
+	{
+		if (currentCharIndex == 0)
+		{
+			System.out.println("You cannot undo any more actions.");
+		}
+		else
+		{
+			currentCharIndex--;
+			userGuesses.remove(cryptogram.getEncryptedChar(currentCharIndex));
+			System.out.println("Last action undone");
+		}
+	}
+	
+	/**
+	 * Undoes all actions.
+	 */
+	private void clear() 
+	{
+		System.out.println("All actions cleared");
+		currentCharIndex = 0;
+		userGuesses.clear();
+	}
+
+	/**
+	 * Determines whether user has completed the game.
+	 */
+	private boolean completed()
+	{
+		if (currentCharIndex == cryptogram.getUniqueChars().size())
+		{
+			if (cryptogram.getCompletionStatus(currentCharIndex, userGuesses))
+			{
+				System.out.println("\n" + cryptogram.getEncryptedPhrase());
+				getMapping();
+				System.out.println("You have successfully completed the cryptogram!");
+					
+				System.exit(0);
+			}
+			else 
+			{
+				System.out.println("You have failed to complete the cryptogram. All values will be now be reset...\n");
+				currentCharIndex = 0;
+				userGuesses.clear();
+			}
+		}
+
+		return false;
+	}
+	
 	/**
 	 * Main method.
 	 * 
@@ -18,6 +186,10 @@ public class Game
 	 */
 	public static void main(String [] args)
 	{
-		generateCryptogram();
+		cryptogram = new Cryptogram("helloworld");
+		Game game = new Game();
+
+		cryptogram.createMapping();
+		game.start();
 	}
 }
